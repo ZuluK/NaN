940b5c61edcce0cfdd8b5028915a023b9419811b
Richard Spence <richardspence@Richards-MacBook-Air.local>
2017-11-16 16:02:54 -0800

    First Commit

--- js/main.js
+++ js/main.js
@@ -1,21 +1,93 @@
-//First create a function named init and put your previous code in the function
-function init (){
-var planet = document.getElementById('greenplanet');
-planet.innerHTML = "Red Alert: hit by phaser fire";
+//Understanding value types
+var x;//undefined
+
+if (x == undefined){
+  //x isn't defined! just deal with it!
 }
 
-window.onload = init;//Assigns the function init to the window.onload property. DO NOT
-                      //use parenthese after the function name. We aren't called the function
-                      //we are just assigning the function value to the window.onload property.
+//How about an object property?
+var customer = {
+  name: 'Jenny'
+};
+if (customer.phoneNumber == undefined) {
+  //get the customer's phone number
+}
 
 
+/* When do I need to check if a variable (or property or array item) is undefined?
 
-//How to set an attribute with setAttribute
-planet.setAttribute('class',"redtext");//the method takes two arguments, the name of the attribute you want to set or change.
+Your code design will dictate this. If you’ve written code so that a property or variable may not have a value when a certain block of code is executed,
+then checking for undefined gives you a way to handle that situation rather than computing with undefined values.
 
 
-//How to GET attributes
-var scoop = document.getElementById('raspberry');
-var altText = scoop.getAttribute('alt');
-console.log('I can"t see the image in the console');
-console.log('but I"m told it looks like' + altText);
+*/
+
+
+/* If undefined is a value, does it have a type?
+
+Yes, it does. The type of undefined is undefined. Why? Well our logic (work with us here) is this: it isn’t an object, or a number or a string or a boolean, or really anything that is defined.
+So why not make the type undefined, too? This is one of those weird twilight zones of JavaScript you just have to accept.
+
+*/
+
+
+//the typeof method in JS
+
+var subject = 'Just a string';
+var probe = typeof subject;
+console.log(probe);
+
+
+//Collect the data from the following experiments
+var test1 = 'abcdefg';
+var test2 = 123;
+var test3 = true;
+var test4 = {};
+var test5 = [];
+var test6;
+var test7 = {"abddef": 123};
+var test8 = ['abdcdef',124];
+function test9 () {return 'abcdefg'};
+
+console.log(typeof test1);
+console.log(typeof test2);
+console.log(typeof test3);
+console.log(typeof test4);
+console.log(typeof test5);
+console.log(typeof test6);
+console.log(typeof test7);
+console.log(typeof test8);
+console.log(typeof test9);
+
+//and example of null
+var test10 = null;
+console.log(test10);
+
+//How to use Null
+
+/*
+There are many functions and methods out there in the world that return objects,
+and you’ll often want to make sure what you’re getting back is a full- fledged object,
+and not null, just in case the function wasn’t able to find one or make one to return to you.
+*/
+
+
+var header = document.getElementById('header');
+if (header == null){
+  //do something here
+  //something is seriously wrong if there's no header
+}
+
+
+/* Keep in mind that getting null doesn’t necessarily mean something is wrong.
+It may just mean something doesn’t exist yet and needs to be created, or something doesn’t exist and you can skip it.
+*/
+
+var weather = document.getElementById('weatherDiv');
+if(weather != null){
+  //create content for the weather div
+  //If the result of getElementById isn’t null, then there is such an element in the page.
+}
+
+
+/* ------ REMEMBER, NULL IS INTENDED TO REPRESENT AN OBJECT THAT ISN'T THERE --*/
